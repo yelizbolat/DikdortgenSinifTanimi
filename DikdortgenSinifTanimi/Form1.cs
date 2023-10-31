142f93d396475875db581cf1c2f43936bd5e79b1
bolat <bolat@YELIZ>
2023-10-31 09:29:36 +0300

    Proje dosyası ekle.

---
+++ DikdortgenSinifTanimi/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DikdortgenSinifTanimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Dikdortgen
+    {
+        private int a, b;
+        public Dikdortgen(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int AlanHesapla()
+        {
+            return a * b;
+        }
+        public int CevreHesapla()
+        {
+            return 2 * (a + b);
+        }
+    }
+}
